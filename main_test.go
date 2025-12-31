218fa37b9c73836aaa4a3d5545099d08edab475b
iamBelugaa <iamnilotpaldeka@gmail.com>
2025-12-31 12:12:56 +0530

    convert test cases to table driven test case

--- main_test.go
+++ main_test.go
@@ -3,30 +3,35 @@ package main
 import "testing"
 
 func TestCountWords(t *testing.T) {
-	input := "one two three four five"
-	wants := 5
-
-	res := CountWords([]byte(input))
-	if res != wants {
-		t.Logf("expected %d, got %d", wants, res)
-		t.Fail()
+	testCases := []struct {
+		name  string
+		input string
+		wants int
+	}{
+		{
+			name:  "5 Words",
+			input: "one two three four five",
+			wants: 5,
+		},
+		{
+			name:  "Empty Input",
+			input: "",
+			wants: 0,
+		},
+		{
+			name:  "Single Space",
+			input: " ",
+			wants: 0,
+		},
 	}
 
-	input = ""
-	wants = 0
-
-	res = CountWords([]byte(input))
-	if res != wants {
-		t.Logf("expected %d, got %d", wants, res)
-		t.Fail()
-	}
-
-	input = " "
-	wants = 0
-
-	res = CountWords([]byte(input))
-	if res != wants {
-		t.Logf("expected %d, got %d", wants, res)
-		t.Fail()
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			res := CountWords([]byte(tc.input))
+			if res != tc.wants {
+				t.Logf("expected %d, got %d", tc.wants, res)
+				t.Fail()
+			}
+		})
 	}
 }
